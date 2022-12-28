e1cebf612d9db984a83d29b7841abf9c0e02e782
Steffan Andrews <stef@orchetect.com>
2022-12-28 01:33:25 -0800

    Added `MarkersExtractor.isAllUniqueIDNonEmpty` unit test

--- Tests/MarkersExtractorTests/MarkersExtractorTests.swift
+++ Tests/MarkersExtractorTests/MarkersExtractorTests.swift
@@ -52,4 +52,50 @@ final class MarkersExtractorTests: XCTestCase {
             extractor.findDuplicateIDs(in: [marker2, marker1, marker2]), [marker2.id(settings.idNamingMode)]
         )
     }
+    
+    func testIsAllUniqueIDNonEmpty_inMarkers() throws {
+        var settings = try MarkersExtractor.Settings(
+            fcpxml: FCPXMLFile(.string("")),
+            outputDir: FileManager.default.temporaryDirectory
+        )
+        settings.idNamingMode = .name
+        
+        let extractor = MarkersExtractor(settings)
+        
+        func makeMarker(_ name: String, position: TCC) -> Marker {
+            Marker(
+                type: .standard,
+                name: name,
+                notes: "",
+                role: "Video",
+                position: try! position.toTimecode(at: ._24),
+                parentInfo: .init(
+                    clipName: "Some Clip",
+                    clipDuration: try! TCC(h: 1).toTimecode(at: ._24),
+                    eventName: "Some Event",
+                    projectName: "MyProject",
+                    libraryName: "MyLibrary"
+                )
+            )
+        }
+        
+        let marker1 = makeMarker("marker1", position: TCC(f: 1))
+        let marker2 = makeMarker("", position: TCC(f: 2))
+        
+        XCTAssertTrue(
+            extractor.isAllUniqueIDNonEmpty(in: [])
+        )
+        
+        XCTAssertTrue(
+            extractor.isAllUniqueIDNonEmpty(in: [marker1])
+        )
+        
+        XCTAssertFalse(
+            extractor.isAllUniqueIDNonEmpty(in: [marker1, marker2])
+        )
+        
+        XCTAssertFalse(
+            extractor.isAllUniqueIDNonEmpty(in: [marker2])
+        )
+    }
 }
