03b831f8d563aef36ec8d01dca94443dddb0eb7e
Steffan Andrews <stef@orchetect.com>
2022-12-30 01:02:39 -0800

    CLI version to "0.2.0-alpha"

--- Sources/markers-extractor-cli/MarkersExtractorCLI.swift
+++ Sources/markers-extractor-cli/MarkersExtractorCLI.swift
@@ -7,7 +7,7 @@ struct MarkersExtractorCLI: ParsableCommand {
     static var configuration = CommandConfiguration(
         abstract: "Tool to extract markers from FCPXML(D).",
         discussion: "https://github.com/TheAcharya/MarkersExtractor",
-        version: "0.1.1"
+        version: "0.2.0-alpha"
     )
     
     @Option(
