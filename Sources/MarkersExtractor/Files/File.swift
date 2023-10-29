5a315ae5163007c83f966f41083b84a038a9f08f
Steffan Andrews <stef@orchetect.com>
2023-10-28 21:10:01 -0700

    Refactored `File` to be more concise

--- Sources/MarkersExtractor/Files/File.swift
+++ Sources/MarkersExtractor/Files/File.swift
@@ -6,29 +6,33 @@
 
 import Foundation
 
-/// Represents a file's contents, either in memory or from a file stored on disk.
+/// Represents a file's contents, either from a file stored on disk or directly from raw data.
 /// In either case, the file content is cached in memory to improve performance and reduce
 /// unnecessary disk activity.
 internal struct File {
-    public var cache: FileContentsCache?
-    public var url: URL?
+    public private(set) var contents: FileContents
+}
+
+extension File {
+    enum FileContents {
+        case fileOnDisk(url: URL, cache: FileContentsCache?)
+        case rawFileContents(FileContentsCache)
+    }
 }
 
 // MARK: - Constructors
 
 extension File {
     init(_ url: URL) {
-        cache = nil
-        self.url = url
+        contents = .fileOnDisk(url: url, cache: nil)
     }
     
     init(fileContents: FileContentsCache) {
-        cache = fileContents
-        url = nil
+        contents = .rawFileContents(fileContents)
     }
     
     static func fileContents(_ contents: FileContentsCache) -> Self {
-        File(cache: contents, url: nil)
+        File(contents: .rawFileContents(contents))
     }
 }
 
@@ -47,7 +51,23 @@ extension File {
     /// Returns a Boolean value indicating whether the file's contents has been read off disk
     /// and cached.
     var isFetched: Bool {
-        cache != nil
+        switch contents {
+        case let .fileOnDisk(_, cache):
+            return cache != nil
+        case .rawFileContents(_):
+            return true
+        }
+    }
+    
+    /// Returns file path URL if instance was constructed from an URL.
+    /// Always returns `nil` if instances was constructed from raw file contents.
+    var url: URL? {
+        switch contents {
+        case let .fileOnDisk(url, _):
+            return url
+        case .rawFileContents(_):
+            return nil
+        }
     }
     
     /// Reads the data off disk and returns a new instance containing the cached data.
@@ -57,52 +77,49 @@ extension File {
     /// the file's contents will also fill the cache if needed.
     ///
     /// - Parameters:
-    ///   - resetCache: Force the cache to reset (flush) and re-read the file's contents from disk.
-    func fetch(resetCache: Bool = false) throws -> Self {
-        var copy = self
-        
-        if resetCache {
-            copy.cache = nil
+    ///   - resetCache: Re-read the file's contents from disk and update the cache regardless of cache state.
+    mutating func fetch(resetCache: Bool = false) throws {
+        switch contents {
+        case let .fileOnDisk(url, cache):
+            if cache != nil, !resetCache { return }
+            do {
+                let data = try Data(contentsOf: url)
+                contents = .fileOnDisk(url: url, cache: .data(data))
+            } catch {
+                throw MarkersExtractorError.extraction(.fileRead(error.localizedDescription))
+            }
+        case .rawFileContents(_):
+            // nothing to fetch and resetting cache is meaningless
+            return
         }
-        
-        if cache != nil {
-            return copy
-        }
-        
-        guard let url = url else {
-            throw MarkersExtractorError.extraction(.fileRead(
-                "File URL is missing while attempting to read file contents."
-            ))
-        }
-        
-        let data = try Data(contentsOf: url)
-        copy.cache = .data(data)
-        return copy
     }
     
     /// Reads the file's contents off disk and returns it as a new `Data` instance.
     /// After the first access, the file's data is cached so subsequent calls are more performant.
-    ///
-    /// - Parameters:
-    ///   - resetCache: Force the cache to reset (flush) and re-read the file's contents from disk.
-    /// - Returns: File contents.
-    func data(resetCache: Bool = false) throws -> Data {
-        let fetched = try fetch(resetCache: resetCache)
-        guard let contents = fetched.cache else {
-            // swiftlint:disable:next force_cast
-            if let url {
+    mutating func data() throws -> Data {
+        // update cache if needed
+        try fetch()
+        
+        // retrieve cache
+        let unwrappedCache: FileContentsCache
+        switch contents {
+        case let .fileOnDisk(url, cache):
+            guard let cache else {
                 throw MarkersExtractorError.extraction(.fileRead(
                     "File could not be read: \(url.path.quoted)."
                 ))
-            } else {
-                throw MarkersExtractorError.extraction(.fileRead(
-                    "File could not be read."
-                ))
             }
+            unwrappedCache = cache
+            
+        case let .rawFileContents(cache):
+            unwrappedCache = cache
         }
-        switch contents {
+        
+        // convert to Data if needed
+        switch unwrappedCache {
         case let .data(data):
             return data
+            
         case let .string(string):
             guard let data = string.data(using: .utf8) else {
                 throw MarkersExtractorError.validation(
