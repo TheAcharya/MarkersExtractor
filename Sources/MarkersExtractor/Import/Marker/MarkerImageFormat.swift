7d6356f2a0b03ff4a39f3169638cc9423794092a
Steffan Andrews <stef@orchetect.com>
2023-10-30 21:08:11 -0700

    Added `name` property to `MarkerImageFormat`, organized code

--- Sources/MarkersExtractor/Import/Marker/MarkerImageFormat.swift
+++ Sources/MarkersExtractor/Import/Marker/MarkerImageFormat.swift
@@ -4,30 +4,21 @@
 //  Licensed under MIT License
 //
 
+// MARK: - MarkerImageFormat
+
 public enum MarkerImageFormat: Equatable, Hashable, Sendable {
     case still(Still)
     case animated(Animated)
-}
-
-extension MarkerImageFormat {
-    public enum Still: String, CaseIterable, Sendable {
-        case png
-        case jpg
+    
+    /// Descriptive name for UI.
+    public var name: String {
+        switch self {
+        case let .still(format):
+            return format.name
+        case let .animated(format):
+            return format.name
+        }
     }
-}
-
-extension MarkerImageFormat.Still: Identifiable {
-    public var id: RawValue { rawValue }
-}
-
-extension MarkerImageFormat {
-    public enum Animated: String, CaseIterable, Sendable {
-        case gif
-    }
-}
-
-extension MarkerImageFormat.Animated: Identifiable {
-    public var id: RawValue { rawValue }
 }
 
 extension MarkerImageFormat: RawRepresentable {
@@ -45,10 +36,10 @@ extension MarkerImageFormat: RawRepresentable {
     
     public var rawValue: String {
         switch self {
-        case let .still(fmt):
-            return fmt.rawValue
-        case let .animated(fmt):
-            return fmt.rawValue
+        case let .still(format):
+            return format.rawValue
+        case let .animated(format):
+            return format.rawValue
         }
     }
 }
@@ -56,9 +47,9 @@ extension MarkerImageFormat: RawRepresentable {
 extension MarkerImageFormat: Identifiable {
     public var id: RawValue {
         switch self {
-        case .still(let format):
+        case let .still(format):
             return "still-\(format.id)"
-        case .animated(let format):
+        case let .animated(format):
             return "animated-\(format.id)"
         }
     }
@@ -74,4 +65,47 @@ extension MarkerImageFormat: CaseIterable {
     public static let allCases: [MarkerImageFormat] =
         Still.allCases.map { .still($0) }
             + Animated.allCases.map { .animated($0) }
+}
+
+// MARK: - MarkerImageFormat: Still
+
+extension MarkerImageFormat {
+    public enum Still: String, CaseIterable, Sendable {
+        case png
+        case jpg
+        
+        /// Descriptive name for UI.
+        public var name: String {
+            switch self {
+            case .png:
+                return "PNG"
+            case .jpg:
+                return "JPEG"
+            }
+        }
+    }
+}
+
+extension MarkerImageFormat.Still: Identifiable {
+    public var id: RawValue { rawValue }
+}
+
+// MARK: - MarkerImageFormat: Animated
+
+extension MarkerImageFormat {
+    public enum Animated: String, CaseIterable, Sendable {
+        case gif
+        
+        /// Descriptive name for UI.
+        public var name: String {
+            switch self {
+            case .gif:
+                return "Animated GIF"
+            }
+        }
+    }
+}
+
+extension MarkerImageFormat.Animated: Identifiable {
+    public var id: RawValue { rawValue }
 }
