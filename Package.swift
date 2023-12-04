b6ca8c94b44b1d37570d1db3de04eb75df2a112c
Steffan Andrews <stef@orchetect.com>
2023-12-04 00:00:35 -0800

    Set minimum requirement to macOS 12.0

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MarkersExtractor",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v12)
     ],
     products: [
         .library(
