dcc4898429fc75428d2d1357ca1c569db7746597
Steffan Andrews <stef@orchetect.com>
2024-01-01 01:49:32 -0800

    Bumped dependency versions

--- Package.swift
+++ Package.swift
@@ -20,11 +20,11 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
+        .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.6"),
         .package(url: "https://github.com/orchetect/TextFileKit.git", from: "0.1.6"),
-        .package(url: "https://github.com/orchetect/TimecodeKit.git", from: "2.0.7"),
+        .package(url: "https://github.com/orchetect/TimecodeKit.git", from: "2.0.8"),
         .package(url: "https://github.com/orchetect/DAWFileKit.git", from: "0.4.0")
     ],
     targets: [
