4fc9915bd327b167fac71ec64770a42b01d2764a
Steffan Andrews <stef@orchetect.com>
2024-01-03 23:33:29 -0800

    Bumped DAWFileKit to 0.4.1 which fixes #81

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.6"),
         .package(url: "https://github.com/orchetect/TextFileKit.git", from: "0.1.6"),
         .package(url: "https://github.com/orchetect/TimecodeKit.git", from: "2.0.8"),
-        .package(url: "https://github.com/orchetect/DAWFileKit.git", from: "0.4.0")
+        .package(url: "https://github.com/orchetect/DAWFileKit.git", from: "0.4.1")
     ],
     targets: [
         .target(
