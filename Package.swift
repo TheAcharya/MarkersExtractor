0d6a0cb9abe54f7294e1a5f8cbb225b67587c612
Steffan Andrews <stef@orchetect.com>
2024-11-25 04:50:38 -0800

    Bumped dependency versions

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
         .package(url: "https://github.com/orchetect/TextFileKit.git", from: "0.2.0"),
         .package(url: "https://github.com/orchetect/TimecodeKit.git", from: "2.3.3"),
         .package(url: "https://github.com/orchetect/DAWFileKit.git", from: "0.5.0"),
-        .package(url: "https://github.com/orchetect/OTCore.git", from: "1.6.2"),
+        .package(url: "https://github.com/orchetect/OTCore.git", from: "1.7.0"),
         .package(url: "https://github.com/orchetect/swift-testing-extensions.git", from: "0.2.0")
     ],
     targets: [
