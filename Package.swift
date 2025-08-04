8b2722b2bec242ef8fb362417449ed4cc7c04d52
Vigneswaran Rajkumar <118706051+IAmVigneswaran@users.noreply.github.com>
2025-08-04 08:54:29 +0800

    Updated XLKit to 1.0.7

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.6.1"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.6.4"),
         .package(url: "https://github.com/apple/swift-collections.git", from: "1.2.1"),
-        .package(url: "https://github.com/TheAcharya/XLKit.git", from: "1.0.6"),
+        .package(url: "https://github.com/TheAcharya/XLKit.git", from: "1.0.7"),
         .package(url: "https://github.com/orchetect/TextFileKit.git", from: "0.2.2"),
         .package(url: "https://github.com/orchetect/TimecodeKit.git", from: "2.3.3"),
         .package(url: "https://github.com/orchetect/DAWFileKit.git", from: "0.5.4"),
