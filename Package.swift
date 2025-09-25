091f5dff880c03e01d14bdb4c1df113dfe736c61
Vigneswaran Rajkumar <118706051+IAmVigneswaran@users.noreply.github.com>
2025-09-25 12:02:11 +0800

    Updated XLKit to 1.0.9
    - Updated other dependencies

--- Package.swift
+++ Package.swift
@@ -23,12 +23,12 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.6.1"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.6.4"),
         .package(url: "https://github.com/apple/swift-collections.git", from: "1.2.1"),
-        .package(url: "https://github.com/TheAcharya/XLKit.git", from: "1.0.8"),
+        .package(url: "https://github.com/TheAcharya/XLKit.git", from: "1.0.9"),
         .package(url: "https://github.com/orchetect/TextFileKit.git", from: "0.2.2"),
-        .package(url: "https://github.com/orchetect/TimecodeKit.git", from: "2.3.3"),
+        .package(url: "https://github.com/orchetect/TimecodeKit.git", from: "2.3.4"),
         .package(url: "https://github.com/orchetect/DAWFileKit.git", from: "0.5.4"),
-        .package(url: "https://github.com/orchetect/OTCore.git", from: "1.7.8"),
-        .package(url: "https://github.com/orchetect/swift-testing-extensions.git", from: "0.2.1")
+        .package(url: "https://github.com/orchetect/OTCore.git", from: "1.7.9"),
+        .package(url: "https://github.com/orchetect/swift-testing-extensions.git", from: "0.2.4")
     ],
     targets: [
         .target(
